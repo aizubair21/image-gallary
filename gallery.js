e68925cfa8cf49b59e220ace288b475e29ef2d72
Amimul ihsan <86657812+amimul21@users.noreply.github.com>
2021-07-17 15:17:42 +0600

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,33 @@
+
+
+let html = '';
+
+for (let index = 1; index <= 15; index++) {
+    html  += `
+        <div class="img" >
+            <img src="images/${index}.jpg" alt="" onclick="openGallery(this)">
+        </div>
+    `;
+}
+document.getElementById('gallery').innerHTML = html;
+
+/*
+var doc = document.querySelector(".img img");
+console.log(doc.length);
+*/
+
+function openGallery(image){
+	
+	var expandArea = document.getElementById("expadedImage");
+	expandArea.src = image.src;
+	var imageText;
+	
+	expandArea.parentElement.style.display = "block";
+	document.getElementById("gallery").style.visibility = "hidden";
+	//alert(image);
+};
+
+function closeGalleryView(){
+	document.getElementById("galleryView").style.display = "none";
+	document.getElementById("gallery").style.visibility = "visible";
+}
